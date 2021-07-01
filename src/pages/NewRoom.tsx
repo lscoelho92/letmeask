2749a6e884070eedf8af16472e9a475965a14c60
Leonardo Coelho <leonardo_soares29@hotmail.com>
2021-07-01 17:01:13 +0200

    Create new Rooms flow

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,6 @@
-import { Link } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { database } from '../services/firebase';
 import { Button } from '../components/Button';
 
 import { useAuth } from '../hooks/useAuth';
@@ -10,7 +12,25 @@ import logoImg from '../assets/images/logo.svg';
 
 
 export function NewRoom() {
-  // const { user } = useAuth();
+  const { user } = useAuth();
+  const [newRoom, setNewRoom] = useState('');
+  const history = useHistory();
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if(newRoom.trim() === '') {
+      return;
+    }
+
+    const roomRef = database.ref('rooms');
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id
+    })
+
+    history.push(`/rooms/${firebaseRoom.key}`);
+  }
 
   return (
     <div id="home-page">
@@ -23,10 +43,12 @@ export function NewRoom() {
         <div className="main-content">
           <img src={logoImg} alt="Letmeask Logo" />
           <h2>Create a new room</h2>
-          <form>
+          <form onSubmit={handleCreateRoom}>
             <input 
               type="text"
               placeholder="Room ID"
+              onChange={event => setNewRoom(event.target.value)}
+              value={newRoom}
             />
             <Button type="submit">
               Create room
